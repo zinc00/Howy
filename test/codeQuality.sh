a6afe185a93cb519a15d1cdfb8425cf93d87d914
avifried <avi@innovid.com>
2015-12-19 17:59:30 +0200

    changed jshint config path and name of file, changed crlf

--- test/codeQuality.sh
+++ test/codeQuality.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-./node_modules/.bin/jshint . --reporter checkstyle -c ../config/jshint-player-config.json > player-jshint.xml
+./node_modules/.bin/jshint . --reporter checkstyle -c ./config/jshint-config.json > player-jshint.xml
 ./node_modules/.bin/jscpd
