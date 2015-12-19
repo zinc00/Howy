e0047273d635b64d10cbf39fbfc9f5ae86b2e777
avifried <avi@innovid.com>
2015-12-19 17:55:53 +0200

    changed jshint config path

--- test/codeQuality.sh
+++ test/codeQuality.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./node_modules/.bin/jshint . --reporter checkstyle -c ./config/jshint-player-config.json > player-jshint.xml
+./node_modules/.bin/jshint . --reporter checkstyle -c ../config/jshint-player-config.json > player-jshint.xml
 ./node_modules/.bin/jscpd
